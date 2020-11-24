9e26ee9f2e086ac49dbc8c447ab73880c29de0dc
miroslavmatovic <v-mirosm@microsoft.com>
2020-11-24 15:40:29 +0100

    initial

---
+++ XamarinAwesomeApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using XamarinAwesomeApp.ViewModels;
+
+namespace XamarinAwesomeApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
